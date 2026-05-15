d6344afb5f3b48379e323474275bf804d51325f3
liuzx <2115469321@qq.com>
2026-05-15 14:50:34 +0800

    fix: correct paths and trigger deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 
-import App from "./app/App.tsx";
-import "./styles/index.css";
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+  createRoot(document.getElementById("root")!).render(<App />);
+  
